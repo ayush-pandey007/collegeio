d08942068cccdcf83a5231582ff7c0c0ec71c669
ayush-pandey007 <ayushpandey1151@gmail.com>
2021-10-25 16:19:27 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <h1 className="text-3xl bg-red-600">Collegeio</h1>
