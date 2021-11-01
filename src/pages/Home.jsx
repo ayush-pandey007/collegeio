a58f78b946a1750dd7245c32782b661a01bf5e32
ashirbad <ashirbadbehera29@gmail.com>
2021-11-01 11:59:30 +0530

    🛂 add logout button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,28 @@
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { signOut } from '@firebase/auth';
 import { auth } from '../Firebase';
 
 const Home = () => {
   const [user] = useAuthState(auth);
+  const logOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log('signout successful');
+      })
+      .catch((err) => {
+        throw new Error(err.message);
+      });
+  };
 
   return (
-    <div>
-      <h1>welcome {user.displayName}</h1>
+    <div className="min-h-screen flex items-center justify-center flex-col gap-7">
+      <h1 className="text-xl">welcome {user.displayName}</h1>
+      <button
+        onClick={logOut}
+        className="bg-green-400 rounded px-4 py-1 text-white"
+      >
+        Logout
+      </button>
     </div>
   );
 };
